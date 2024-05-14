51d536257fdbb2588692cb050ecda7715d62012e
Luke Repko <luke.repko@rackspace.com>
2024-05-13 23:10:09 -0500

    fix: permit all local tcp/udp within subnet
    Allow all tcp/udp between the flex-nodes. At least etcd is trying to use
    the management network instead of the openstack-flex-internal net.
    Possibly other things will be trying to comm as well.
    
    Ultimately I don't like the idea of letting externally translated web
    traffic onto the same broadcast network. We are doing that for SJC, and
    the last few commits is another iteration of that, but ideally we would
    segregate that traffic better.
    
    I may introduce another L2 network solely to seperate out public facing
    traffic from management net. Maybe it's more trouble than it's worth
    though?

--- main.tf
+++ main.tf
@@ -64,15 +64,23 @@ resource "openstack_networking_secgroup_v2" "secgroup-flex-nodes" {
   name = "openstack-flex-nodes"
 }
 
-## Create management network security group node rule for local SSH access
+## Create management network security group node rule for local TCP access
 # Create sec group rule
-resource "openstack_networking_secgroup_rule_v2" "secgroup-rule-flex-node-local-ssh" {
+resource "openstack_networking_secgroup_rule_v2" "secgroup-rule-flex-node-permit-tcp-local" {
   direction         = "ingress"
   ethertype         = "IPv4"
   security_group_id = openstack_networking_secgroup_v2.secgroup-flex-nodes.id
   protocol          = "tcp"
-  port_range_min    = "22"
-  port_range_max    = "22"
+  remote_ip_prefix  = "172.31.0.0/22"
+}
+
+## Create management network security group node rule for local UDP access
+# Create sec group rule
+resource "openstack_networking_secgroup_rule_v2" "secgroup-rule-flex-node-permit-udp-local" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  security_group_id = openstack_networking_secgroup_v2.secgroup-flex-nodes.id
+  protocol          = "udp"
   remote_ip_prefix  = "172.31.0.0/22"
 }
 
