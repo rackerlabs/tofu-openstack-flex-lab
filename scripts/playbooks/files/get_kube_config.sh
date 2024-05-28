1b3c775c4f8b0879ff03aed2e716b582e2369458
Chris Blumentritt <chris.blumentritt@rackspace.com>
2024-05-28 16:16:41 -0500

    Add script for retrieving kube config
    This copies the `get_kube_config.sh` script to the launcher node. . After running kubespray use this script to copy the kube config to the ubuntu user on the launcher node.
    
    Fixes a bunch of ansible-lint complaints

---
+++ scripts/playbooks/files/get_kube_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Simple script to run from launcher node to get the kube config from the kubernetes node
+IP_ADDRESS=$(getent hosts  kubernetes01 | awk '{print $1}')
+rsync -avz --rsync-path="sudo rsync" kubernetes01:/root/.kube $HOME/
+sed -i "s/127.0.0.1/$IP_ADDRESS/g" $HOME/.kube/config
