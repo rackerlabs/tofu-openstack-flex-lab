6479d74e39210afda858d678b55b059ea47160bd
Chris Blumentritt <chris.blumentritt@rackspace.com>
2024-06-28 14:41:36 -0500

    Adding script quickly run the steps up to cluster deployment
    Signed-off-by: Chris Blumentritt <chris.blumentritt@rackspace.com>

---
+++ scripts/playbooks/files/genestack_deploy_cluster.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bas[h
+ansible   -m shell -a 'hostnamectl set-hostname {{ inventory_hostname }}' --become all
+ansible - -m shell -a "grep 127.0.0.1 /etc/hosts | grep -q {{ inventory_hostname }} || sed -i 's/^127.0.0.1.*/127.0.0.1 {{ inventory_hostname }} localhost.localdomain localhost/' /etc/hosts" --become all
+cd /opt/genestack/ansible/playbooks && ansible-playbook host-setup.yml
+cd /opt/genestack/submodules/kubespray && ansible-playbook cluster.yml -b
+get_kube_config.sh
