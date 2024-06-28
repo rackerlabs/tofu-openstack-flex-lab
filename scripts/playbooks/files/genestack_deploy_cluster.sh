43d6c73096c0e57075cb8bde85f8322577686341
Chris Blumentritt <chris.blumentritt@rackspace.com>
2024-06-28 16:37:48 -0500

    Fix some typos

--- scripts/playbooks/files/genestack_deploy_cluster.sh
+++ scripts/playbooks/files/genestack_deploy_cluster.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bas[h
+#!/usr/bin/env bash
 ansible   -m shell -a 'hostnamectl set-hostname {{ inventory_hostname }}' --become all
 ansible - -m shell -a "grep 127.0.0.1 /etc/hosts | grep -q {{ inventory_hostname }} || sed -i 's/^127.0.0.1.*/127.0.0.1 {{ inventory_hostname }} localhost.localdomain localhost/' /etc/hosts" --become all
 cd /opt/genestack/ansible/playbooks && ansible-playbook host-setup.yml
