4d873ebf4f32c453542aaaa297ce2d20550a8781
Chris Blumentritt <chris.blumentritt@rackspace.com>
2024-07-03 16:59:45 -0500

    Add forks and timeoute to cluster.yaml command

--- scripts/playbooks/files/genestack_deploy_cluster.sh
+++ scripts/playbooks/files/genestack_deploy_cluster.sh
@@ -2,5 +2,5 @@
 ansible -m shell -a 'hostnamectl set-hostname {{ inventory_hostname }}' --become all
 ansible -m shell -a "grep 127.0.0.1 /etc/hosts | grep -q {{ inventory_hostname }} || sed -i 's/^127.0.0.1.*/127.0.0.1 {{ inventory_hostname }} localhost.localdomain localhost/' /etc/hosts" --become all
 cd /opt/genestack/ansible/playbooks && ansible-playbook host-setup.yml
-cd /opt/genestack/submodules/kubespray && ansible-playbook cluster.yml -b
+cd /opt/genestack/submodules/kubespray && ansible-playbook cluster.yml -b -f 30 -T 30
 get_kube_config.sh
